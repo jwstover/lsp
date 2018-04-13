fabd5619ced92a7d6b50c7ec90fd3a21a43e37ec
Jake Stover <jwstover@missionfocus.com>
2018-04-12 22:59:04 -0500

    Added the beginning of command line arguments

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,40 @@
 //
 #include <iostream>
 #include <string>
-#include <teem/nrrd.h>
 #include <boost/program_options.hpp>
 
 namespace po = boost::program_options;
 
 int main(int argc, char** argv) {
-  po::options_description desc("Allowed Options");
-  desc.add_options()
-          ("help", "print help")
-          ("input-file,i", "input nrrd file");
+  po::options_description desc("Usage Information:");
+  try {
+    desc.add_options()
+            ("help,h", "help message here...")
+            ("config-file,c", "configuration file");
 
-  Nrrd *nin = nrrdNew();
+    po::variables_map vm;
 
+    try {
+      po::store(po::parse_command_line(argc, argv, desc), vm);
+
+      if (vm.count("help") || vm.empty()) {
+        std::cout << "Lightsheet Processing Application" << std::endl
+                  << desc << std::endl;
+        return 0;
+      }
+
+      po::notify(vm);
+    } catch (po::error &e) {
+      std::cerr << "ERROR: " << e.what() << std::endl << std::endl;
+      std::cerr << desc << std::endl;
+      return 1;
+    }
+
+  } catch (std::exception &e) {
+    std::cerr << "Unhandled Exception reached the top of main:"
+                 << e.what() << ", application will exit" << std::endl;
+    return 2;
+  }
+
+  return 0;
 }
